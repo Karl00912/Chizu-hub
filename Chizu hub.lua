fef441ccdd5015b52d4a1f744324f407f86e635d
Karl00912 <ortizkarlisback@gmail.com>
2025-01-03 22:02:27 +0800

    Update Chizu hub.lua

--- Chizu hub.lua
+++ Chizu hub.lua
@@ -2,7 +2,7 @@ local scripts = {
     [3101667897] = "https://raw.githubusercontent.com/Karl00912/Chizu-hub/refs/heads/main/legend%20of%20speed.lua",
     [10253248401] = "https://raw.githubusercontent.com/Karl00912/Chizu-hub/refs/heads/main/elementals%20powers%20tycoon.lua",
     [76098497356508] = "https://raw.githubusercontent.com/Karl00912/Chizu-hub/refs/heads/main/fit%20piece%202.lua",
-    [7540891731] = "https://raw.githubusercontent.com/Karl00912/Chizu-hub/refs/heads/main/Red%20Light%20Green%20Light.lua"
+    [7577073048] = "https://raw.githubusercontent.com/Karl00912/Chizu-hub/refs/heads/main/Red%20Light%20Green%20Light.lua"
 }
 
 local placeId = game.PlaceId
